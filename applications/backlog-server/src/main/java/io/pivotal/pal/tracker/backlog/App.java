23432723380ccf3dec63c590b9376db56732fe85
kumarp85 <mana.santosh9991@gmail.com>
2019-08-22 17:55:49 +0530

    Service Discovery and Client Side Load Balancing v1

--- applications/backlog-server/src/main/java/io/pivotal/pal/tracker/backlog/App.java
+++ applications/backlog-server/src/main/java/io/pivotal/pal/tracker/backlog/App.java
@@ -1,29 +1,14 @@
-package io.pivotal.pal.tracker.backlog;
+package io.pivotal.pal.tracker.eurekaserver;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.client.RestOperations;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-import java.util.TimeZone;
-
-
+@EnableEurekaServer
 @SpringBootApplication
-@ComponentScan({"io.pivotal.pal.tracker.backlog", "io.pivotal.pal.tracker.restsupport"})
 public class App {
 
     public static void main(String[] args) {
-        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
         SpringApplication.run(App.class, args);
-    }
-
-    @Bean
-    ProjectClient projectClient(
-        RestOperations restOperations,
-        @Value("${registration.server.endpoint}") String registrationEndpoint
-    ) {
-        return new ProjectClient(restOperations, registrationEndpoint);
     }
 }
--- applications/registration-server/src/main/java/io/pivotal/pal/tracker/registration/App.java
+++ applications/registration-server/src/main/java/io/pivotal/pal/tracker/registration/App.java
@@ -1,23 +1,14 @@
-package io.pivotal.pal.tracker.registration;
+package io.pivotal.pal.tracker.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-import java.util.TimeZone;
-
-
+@EnableEurekaServer
 @SpringBootApplication
-@ComponentScan({
-    "io.pivotal.pal.tracker.accounts",
-    "io.pivotal.pal.tracker.restsupport",
-    "io.pivotal.pal.tracker.projects",
-    "io.pivotal.pal.tracker.users",
-    "io.pivotal.pal.tracker.registration"
-})
 public class App {
+
     public static void main(String[] args) {
-        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
         SpringApplication.run(App.class, args);
     }
 }
